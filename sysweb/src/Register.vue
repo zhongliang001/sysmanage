fbe2b1da018edba7f617561d8f7719ef945fec91
zhongliang <coolzhongliang@126.com>
2021-01-25 20:02:49 +0800

    公共代码移走

--- sysweb/src/Register.vue
+++ sysweb/src/Register.vue
@@ -1,14 +1,14 @@
 <template>
     <div>
-        <zl-form ref="register" column="1" :method="method" :url="url">
-            <zl-item type="text" name="username" field-name="用户名"/>
-            <zl-item type="text" name="realName" field-name="真实姓名"/>
-            <zl-item type="select" name="certType" field-name="证件类型" dict-name="CERT_TYPE"/>
-            <zl-item type="text" name="certNo" field-name="证件号码"/>
-            <zl-item type="select" name="sex" field-name="性别" dict-name="SEX"/>
-            <zl-item type="text" name="phoneNo" field-name="手机号码"/>
-            <zl-item type="password" name="password" field-name="密码"/>
-            <zl-item type="password" name="repassword" field-name="再输一次密码"/>
+        <zl-form ref="register" column="1" :method="method" :url="url" :validate="validate">
+            <zl-item type="text" name="username" field-name="用户名" required="true"/>
+            <zl-item type="text" name="realName" field-name="真实姓名" required="true"/>
+            <zl-item type="select" name="certType" field-name="证件类型" dict-name="CERT_TYPE" required="true"/>
+            <zl-item type="text" name="certNo" field-name="证件号码" required="true"/>
+            <zl-item type="select" name="sex" field-name="性别" dict-name="SEX" required="true"/>
+            <zl-item type="text" name="phoneNo" field-name="手机号码" required="true"/>
+            <zl-item type="password" name="password" field-name="密码" required="true"/>
+            <zl-item type="password" name="repassword" field-name="再输一次密码" required="true"/>
         </zl-form>
         <zl-button type="submit" @click.native="onclick" name="注册"></zl-button>
     </div>
@@ -16,7 +16,6 @@
 </template>
 
 <script>
-    import zlaxios from "./lib/zlaxios";
 
     export default {
         name: "Register",
@@ -30,18 +29,32 @@
             onclick: function () {
                 let _this = this
                 let form = _this.common.getForm(this,'register')
+                if(form.checkAll()){
+                    let reqData = form.reqData
+                    this.zlaxios.request({
+                        url: _this.zlService.baseUrl + _this.url,
+                        data: reqData,
+                        method: _this.method,
+                        success: function (response) {
+                            alert(response.msg)
+                        },
+                        error: function (error) {
+                            alert(error.msg)
+                        }
+                    })
+                }
+            },
+            validate: function () {
+                let form = this.common.getForm(this,'register')
                 let reqData = form.reqData
-                zlaxios.request({
-                    url: _this.url,
-                    data: reqData,
-                    method: _this.method,
-                    success: function (response) {
-                        alert(response.msg)
-                    },
-                    error: function (error) {
-                        alert(error.msg)
-                    }
-                })
+                let password = reqData.password
+                let repassword = reqData.repassword
+                if(password !== repassword){
+                    alert('两次输入的密码不一样')
+                    return false
+                }else{
+                    return true
+                }
             }
         }
     }
--- sysweb/src/index.js
+++ sysweb/src/index.js
@@ -1,15 +1,10 @@
 /*jslint es6, white, browser */
 import Vue from 'vue'
 import App from './App.vue'
-import common from './lib/common.js'
-import zlaxios from "./lib/zlaxios"
 import zolui from 'zolui'
 import VueRouter from "vue-router";
-import routes  from './lib/routers'
-
-
-// import Login from "./Login";
-// import Register from "./Register";
+import routes from './lib/routers'
+import zlService from "./lib/zlservice";
 
 Vue.use(VueRouter);
 const originalPush = VueRouter.prototype.push
@@ -23,38 +18,28 @@ const router = new VueRouter({
 Vue.config.devtools = true
 Vue.use(zolui)
 
-
-if(typeof(Vue)=="function"){
-    Vue.prototype.common=common
-    // axios.request('/sdict/sdictTree').then(result =>{
-    //     let dictData = result.data
-    //     if(dictData){
-    //         Vue.prototype.dictData = dictData
-    //     }
-    //     new Vue({
-    //         render: h => h(Login)
-    //     }).$mount('#app')
-    //
-    // })
-    zlaxios.request({
-        url: '/sdict/sdictTree',
-        success: function (response) {
-            let dictData = response.data
-                if(dictData){
-                    Vue.prototype.dictData = dictData
-                }
-                new Vue({
-                    router,
-                    render: h => h(App)
-                }).$mount('#app')
-        },
-        error: function (error) {
-            new Vue({
-                router,
-                render: h => h(App)
-            }).$mount('#app')
-        }
-    })
-
-    Vue.prototype.zlaxios = zlaxios
+if (typeof (Vue) == "function") {
+    Vue.prototype.zlService = zlService
 }
+
+// Vue.prototype.common=common
+Vue.prototype.zlaxios.request({
+    url: zlService.baseUrl + '/sdict/sdictTree',
+    success: function (response) {
+        let dictData = response.data
+        if (dictData) {
+            Vue.prototype.dictData = dictData
+        }
+        new Vue({
+            router,
+            render: h => h(App)
+        }).$mount('#app')
+    },
+    error: function (error) {
+        new Vue({
+            router,
+            render: h => h(App)
+        }).$mount('#app')
+    }
+})
+
