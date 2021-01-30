7cbade58c253ffec70b91d69c4a691b263e189ca
zhongliang <coolzhongliang@126.com>
2021-01-30 16:16:56 +0800

    注册中心建立

---
+++ sysweb/src/pages/Hi.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>hi</h1>
+</template>
+
+<script>
+    export default {
+        name: "Hi",
+        created: function() {
+            console.log('hi')
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
