bcb95afbcea5d79b5a7d6f3a64ae8753a51c9ca4
zhongliang <coolzhongliang@126.com>
2021-06-23 22:45:31 +0800

    数据字典查询，及其他功能优化

--- sysweb/src/pages/Dict.vue
+++ sysweb/src/pages/Dict.vue
@@ -1,41 +1,69 @@
 <template>
-    <div>
-        <zl-page :viewPage="viewPage" page="query">
-            <zl-query-table re="dict" method="post" column="1" :fileds="fileds" :url="url" :titles="titles">
-
-            </zl-query-table>
-        </zl-page>
-    </div>
+  <div>
+    <zl-page :viewPage="viewPage" page="query">
+      <zl-query-table ref="dict" method="post" :fileds="fileds" :url="url" :titles="titles">
+        <zl-button type="button" name="查看" @click.native="view"></zl-button>
+      </zl-query-table>
+    </zl-page>
+    <zl-page :viewPage="viewPage" page="detail">
+      <zl-f-table ref="fTable" :column="2" :req-data="reqData">
+        <zl-item type="text" field-name="数据字典类型" name="dictType"/>
+        <zl-item type="text" field-name="数据字典描述" name="dictDesc"/>
+      </zl-f-table>
+      <zl-query-table :isShow="isShow" ref="dictDetail" method="post" :fileds="detailFileds" :titles="detailTitles" :url="url1">
+        <zl-button type="button" name="查看" @click.native="view"></zl-button>
+      </zl-query-table>
+    </zl-page>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'Dict',
-        data: function () {
-            return {
-                viewPage: "query",
-                fileds: [
-                    {
-                        type: 'text',
-                        cnName: "数据字典描述",
-                        name: "dictDesc"
-                    }
-                ],
-                titles: [
-                    {
-                        cnName: "数据字典类型",
-                        name: "dictType"
-                    },
-                    {
-                        cnName: "数据字典描述",
-                        name: "dictDesc"
-                    }
-                ],
-                url: this.zlService.baseUrl + '/dict/selectGroup',
-                reqData:{}
-            }
+export default {
+  name: 'Dict',
+  data: function () {
+    return {
+      viewPage: "query",
+      fileds: [
+        {
+          type: 'text',
+          cnName: "数据字典描述",
+          name: "dictDesc"
         }
+      ],
+      titles: [
+        {
+          cnName: "数据字典类型",
+          name: "dictType"
+        },
+        {
+          cnName: "数据字典描述",
+          name: "dictDesc"
+        }
+      ],
+      detailFileds:[
+
+      ],
+      detailTitles:[
+
+      ],
+      url: this.zlService.baseUrl + '/dict/selectGroup',
+      url1: this.zlService.baseUrl + '/dict/select',
+      reqData: {},
+      isShow: false
     }
+  },
+  methods: {
+    view: function () {
+      let _this = this
+      let table = _this.common.getComponent(this, 'dict')
+      this.reqData = table.selData
+      this.viewPage = 'detail'
+      let detailTable = _this.common.getComponent(this, 'dictDetail')
+      detailTable.setReqData(this.reqData)
+      detailTable.query()
+    }
+  }
+}
 </script>
 
 <style scoped>
--- sysweb/src/pages/Menu.vue
+++ sysweb/src/pages/Menu.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <zl-page :viewPage="viewPage" page="query">
-            <zl-query-table column="2" re="menu" method="post" :url="url" :fileds="fileds" :titles="titles">
+            <zl-query-table ref="table" column="2" re="menu" method="post" :url="url" :fileds="fileds" :titles="titles">
                 <zl-button type="button" name="新增"  @click.native="add"></zl-button>
                 <zl-button type="button" name="修改"  @click.native="update"></zl-button>
                 <zl-button type="button" name="查看"  @click.native="view"></zl-button>
